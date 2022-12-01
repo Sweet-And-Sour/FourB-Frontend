6c1f3154104248eb86382f3e5b1577d6ef62d380
YeJun, Jung <yejun614@naver.com>
2022-12-01 23:52:56 +0900

    Feat: Intro Page 추가

---
+++ pages/Intro.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <FourBLogo />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import FourBLogo from '~/components/FourBLogo.vue';
+
+export default defineComponent({
+    components: { FourBLogo },
+    setup() {
+        return {};
+    },
+})
+</script>
+
+<style scoped>
+
+</style>
