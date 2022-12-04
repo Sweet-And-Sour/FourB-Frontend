a5c07e0b71c05383a396757328096b52b3d6eb28
YeJun, Jung <yejun614@naver.com>
2022-12-05 01:02:07 +0900

    Feat: Profile page 추가

--- pages/profile.vue
+++ pages/profile.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="profile-page">
-
+    <GlobalNavigationBar />
   </div>
 </template>
 
@@ -10,6 +10,61 @@ import { defineComponent } from 'vue'
 export default defineComponent({
   setup () {
     return {}
+  },
+  data () {
+    return {
+      profile: {
+        backgroundImage: '',
+        avatarImage: '',
+        username: '',
+        introdunction: '',
+        field: '',
+        contact: {
+          email: '',
+          website: '',
+        },
+      },
+      teams: [
+        {
+          thumbnailImage: '',
+          title: '',
+          teamName: '',
+          link: '',
+        },
+        {
+          thumbnailImage: '',
+          title: '',
+          teamName: '',
+          link: '',
+        },
+      ],
+      worksCategory: [
+        {
+          icon: '',
+          name: '',
+        }
+      ],
+      works: [
+        {
+          id: 0,
+          thumbnailImage: '',
+          title: '',
+          author: '',
+          description: '',
+          link: '',
+        },
+      ],
+      likes: [
+        {
+          id: 0,
+          thumbnailImage: '',
+          title: '',
+          author: '',
+          description: '',
+          link: '',
+        },
+      ],
+    }
   }
 })
 </script>
