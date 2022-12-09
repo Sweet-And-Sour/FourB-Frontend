5fcc57088903d457ceae187e6cb16b2be9272207
YeJun, Jung <yejun614@naver.com>
2022-12-10 05:54:27 +0900

    Fix: refresh token 생성을 위한 component로 변경

--- components/AuthChecker.vue
+++ components/AuthChecker.vue
@@ -1,82 +1,21 @@
 <template>
-  <div>
-  </div>
+  <div></div>
 </template>
 
 <script lang="ts">
-import axios from 'axios'
-import Cookies from 'js-cookie'
 import { defineComponent } from 'vue'
 
 export default defineComponent({
-  props: {
-    isSign: {
-      type: Boolean,
-      default: false,
-    },
-    username: {
-      type: String,
-      default: '',
-    }
-  },
   mounted () {
-    setInterval(() => this.authCheck(), 100);
+    this.refreshToken();
     setInterval(() => this.refreshToken(), 30000);
   },
   methods: {
-    authCheck ()  {
-      const token = Cookies.get('accessToken');
-
-      if (this.isSign && token === undefined) {
-        this.$emit('update:isSign', false);
-
-      } else if (!this.isSign && token !== undefined) {
-        this.$emit('update:isSign', true);
-
-        axios
-        .get('/api/sign/payload', {
-          headers: {
-            accept: '*/*',
-            'Authorization': `Bearer ${token}`
-          }
-        })
-        .then((response) => {
-          this.$emit('update:username', response.data.username);
-        })
-        .catch(() => {
-          Cookies.remove('accessToken');
-          this.$emit('update:isSign', false);
-          this.$emit('update:username', '');
-        });
-      }
-    },
     refreshToken () {
-      const token = Cookies.get('accessToken');
-
-      if (token === undefined) {
-        return;
+      if (this.$accessor.accessToken != null && this.$accessor.accessToken !== '') {
+        this.$accessor.refreshToken();
       }
-
-      axios
-        .get('/api/sign/refresh', {
-          headers: {
-            accept: '*/*',
-            'Authorization': `Bearer ${token}`
-          }
-        })
-        .then((response) => {
-          Cookies.set('accessToken', response.data.access_token);
-        })
-        .catch(() => {
-          Cookies.remove('accessToken');
-          this.$emit('update:isSign', false);
-          this.$emit('update:username', '');
-        });
     }
   },
 })
 </script>
-
-<style scoped>
-
-</style>
