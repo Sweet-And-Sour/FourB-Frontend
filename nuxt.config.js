f67d17239a487d7ee432d764c84111520018613b
YeJun, Jung <yejun614@naver.com>
2022-12-09 15:50:54 +0900

    Fix: Nuxt Mode를 SPA로 변경

--- nuxt.config.js
+++ nuxt.config.js
@@ -5,7 +5,8 @@ export default {
   },
 
   // Disable server-side rendering: https://go.nuxtjs.dev/ssr-mode
-  ssr: false,
+  // ssr: false,
+  mode: 'spa',
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
