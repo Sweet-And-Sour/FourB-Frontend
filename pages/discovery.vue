c62ba4a14089b7b34a34df9e075e987afb5018e2
YeJun, Jung <yejun614@naver.com>
2022-12-12 07:43:40 +0900

    Feat: Discovery page 컨텐츠 가져오는 기능 추가

--- pages/discovery.vue
+++ pages/discovery.vue
@@ -67,29 +67,27 @@
         </div>
 
         <content>
-          <div v-for="(column, index) in artworks" :key="index" class="column">
-            <div
-              v-for="item in column"
-              :key="item.id"
-              class="item"
-            >
-              <a :href="item.link">
-                <div class="thumbnail">
-                  <img :src="item.thumbnailImage" alt="thumbnail" />
+          <div v-for="(item, index) in portfolio" :key="index" class="item">
+            <a :href="item.link">
+              <div class="thumbnail">
+                <div class="thumbnail-image" :style="`background-image: url(${item.thumbnail})`"></div>
 
-                  <div class="top">
-                    <button :data-id="item.id" @click.prevent="artworkLikeBtn"><i class="bi bi-heart"></i></button>
-                    <span>{{ item.viewCount }}</span>
-                  </div>
+                <div class="top">
+                  <button :data-id="item.id" @click.prevent="artworkLikeBtn"><i class="bi bi-heart"></i></button>
+                  <span>{{ item.like }}</span>
                 </div>
+              </div>
 
-                <div class="info">
-                  <span class="title">{{ item.title }}</span>
-                </div>
-              </a>
-            </div>
+              <div class="info">
+                <span class="title">{{ item.title }}</span>
+              </div>
+            </a>
           </div>
         </content>
+
+        <div id="more-contents-btn">
+          <button class="btn btn-outline-secondary" type="button" @click="getContents">More Contents</button>
+        </div>
       </section>
     </main>
 
@@ -139,95 +137,24 @@ export default defineComponent({
           link: '#',
         },
       ],
-      artworks: [
-        [
-          {
-            id: 0,
-            thumbnailImage: 'https://images.unsplash.com/photo-1525547719571-a2d4ac8945e2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1528&q=80',
-            title: 'Macbook Pro',
-            viewCount: 123,
-            link: '#',
-          },
-          {
-            id: 1,
-            thumbnailImage: 'https://images.unsplash.com/photo-1499951360447-b19be8fe80f5?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2670&q=80',
-            title: 'Working',
-            viewCount: 123,
-            link: '#',
-          },
-          {
-            id: 2,
-            thumbnailImage: 'https://images.unsplash.com/photo-1544731612-de7f96afe55f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2670&q=80',
-            title: 'Asus Laptop',
-            viewCount: 123,
-            link: '#',
-          },
-        ],
-        [
-          {
-            id: 3,
-            thumbnailImage: 'https://images.unsplash.com/photo-1547394765-185e1e68f34e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2670&q=80',
-            title: 'Keyboard',
-            viewCount: 123,
-            link: '#',
-          },
-          {
-            id: 4,
-            thumbnailImage: 'https://images.unsplash.com/photo-1664575197229-3bbebc281874?ixlib=rb-4.0.3&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2670&q=80',
-            title: 'Windows',
-            viewCount: 123,
-            link: '#',
-          },
-          {
-            id: 5,
-            thumbnailImage: 'https://images.unsplash.com/photo-1551739440-5dd934d3a94a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1528&q=80',
-            title: 'Google',
-            viewCount: 123,
-            link: '#',
-          },
-          {
-            id: 9,
-            thumbnailImage: 'https://images.unsplash.com/photo-1665686310974-2ed1eb7f57ac?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2670&q=80',
-            title: 'Green',
-            viewCount: 123,
-            link: '#',
-          },
-        ],
-        [
-          {
-            id: 6,
-            thumbnailImage: 'https://images.unsplash.com/photo-1471897488648-5eae4ac6686b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1287&q=80',
-            title: 'Green',
-            viewCount: 123,
-            link: '#',
-          },
-          {
-            id: 7,
-            thumbnailImage: 'https://images.unsplash.com/photo-1665686307516-1915b9616526?ixlib=rb-4.0.3&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2670&q=80',
-            title: 'Surface',
-            viewCount: 123,
-            link: '#',
-          },
-          {
-            id: 8,
-            thumbnailImage: 'https://images.unsplash.com/photo-1551739440-5dd934d3a94a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1528&q=80',
-            title: 'Google',
-            viewCount: 123,
-            link: '#',
-          },
-          {
-            id: 10,
-            thumbnailImage: 'https://images.unsplash.com/photo-1665686307516-1915b9616526?ixlib=rb-4.0.3&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2670&q=80',
-            title: 'Surface',
-            viewCount: 123,
-            link: '#',
-          },
-        ],
+      contentPage: 0,
+      orderBy: 'created_datetime',
+      portfolio: [
+        {
+          id: 0,
+          thumbnail: 'https://images.unsplash.com/photo-1525547719571-a2d4ac8945e2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1528&q=80',
+          title: 'Macbook Pro',
+          link: '#',
+          like: 0,
+        },
       ],
     }
   },
   mounted () {
     this.getCategories();
+
+    this.portfolio = [];
+    this.getContents({});
   },
   methods: {
     getCategories () {
@@ -242,8 +169,24 @@ export default defineComponent({
         .get('/api/category', config)
         .then((response) => {
           this.categories = response.data.results;
-          console.log(this.categories);
         })
+    },
+    getContents (_event: any) {
+      axios
+        .get(`/api/content/all?page=${this.contentPage}&orderBy=${this.orderBy}`)
+        .then((response) => {
+          if (response.data.success) {
+            const portfolio = [...this.portfolio, ...response.data.contents];
+            this.portfolio = portfolio;
+          }
+        })
+        .catch((error) => {
+          alert('컨텐츠 가져오는데 오류가 발생했습니다!');
+          console.error(error);
+        });
+
+      this.contentPage++;
+      return undefined;
     },
     artworkLikeBtn (_event: any) {
       return undefined;
@@ -324,6 +267,7 @@ export default defineComponent({
   #artwork content {
     display: flex;
     flex-direction: row;
+    flex-wrap: wrap;
     align-items: flex-start;
     justify-content: center;
   }
@@ -332,16 +276,20 @@ export default defineComponent({
     transition: all 500ms ease;
   }
 
-  #artwork content .column {
+  /* #artwork content .column {
     display: flex;
     flex-direction: column;
     align-items: center;
     justify-content: flex-start;
-  }
+  } */
 
   #artwork content .item {
+    width: 380px;
+    min-height: 300px;
     position: relative;
     margin: 10px;
+    display: flex;
+    flex-direction: column;
   }
 
   #artwork content .item a {
@@ -349,13 +297,10 @@ export default defineComponent({
     text-decoration: blink;
   }
 
-  #artwork content .item .thumbnail {
-    width: 100%;
-  }
-
-  #artwork content .item img {
-    max-width: 100%;
-    /* width: 100%; */
+  #artwork content .item .thumbnail-image {
+    width: 380px;
+    height: 300px;
+    background-position: center;
     border-radius: 20px;
     object-fit: scale-down;
   }
@@ -419,5 +364,10 @@ export default defineComponent({
 
   #artwork content .item:hover .title {
     color: black;
+  }
+
+  #more-contents-btn {
+    width: fit-content;
+    margin: 40px auto;
   }
 </style>
